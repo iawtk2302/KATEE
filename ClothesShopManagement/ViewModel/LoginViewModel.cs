17a265f4c1863376c66c6929c15eecd6f20ce0a2
iawtk2302 <86036919+iawtk2302@users.noreply.github.com>
2021-10-30 09:20:33 +0700

    Phien ban dau tien

---
+++ ClothesShopManagement/ViewModel/LoginViewModel.cs
@@ -0,0 +1,44 @@
+﻿using ClothesShopManagement.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace ClothesShopManagement.ViewModel
+{
+    public class LoginViewModel : BaseViewModel
+    {
+        public ICommand CloseLogin { get; set; }
+        public ICommand MinimizeLogin { get; set; }
+        public ICommand MoveLogin { get; set; }
+        public ICommand Login { get; set; }
+        public LoginViewModel()
+        {
+            CloseLogin = new RelayCommand<LoginWindow>((p) => true, (p) => Close());
+            MinimizeLogin = new RelayCommand<LoginWindow>((p) => true, (p) => Minimize(p));
+            MoveLogin = new RelayCommand<LoginWindow>((p) => true, (p) => Move(p));
+            Login = new RelayCommand<LoginWindow>((p) => true, (p) => login(p));
+        }
+        public void Close()
+        {
+            System.Windows.Application.Current.Shutdown();
+        }
+        public void Minimize(LoginWindow p)
+        {
+            p.WindowState = WindowState.Minimized;
+        }
+        public void Move(LoginWindow p)
+        {
+            p.DragMove();
+        }
+        public void login(LoginWindow p)
+        {
+            MainWindow mainWindow = new MainWindow();
+            p.Close();
+            mainWindow.ShowDialog();
+        }
+    }
+}
---
+++ ClothesShopManagement/ViewModel/MainViewModel.cs
@@ -0,0 +1,63 @@
+﻿using ClothesShopManagement.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace ClothesShopManagement.ViewModel
+{
+    public class MainViewModel : BaseViewModel
+    {
+        public ICommand CloseLogin { get; set; }
+        public ICommand MinimizeLogin { get; set; }
+        public ICommand MoveWindow { get; set; }
+        public ICommand GetIdTab { get; set; }
+        public ICommand SwitchTab { get; set; }
+        public string Name;
+        public MainViewModel()
+        {
+            CloseLogin = new RelayCommand<MainWindow>((p) => true, (p) => Close());
+            MinimizeLogin = new RelayCommand<MainWindow>((p) => true, (p) => Minimize(p));
+            MoveWindow = new RelayCommand<MainWindow>((p) => true, (p) => moveWindow(p));
+            GetIdTab = new RelayCommand<RadioButton>((p) => true, (p) => Name = p.Uid);
+            SwitchTab = new RelayCommand<MainWindow>((p) => true, (p) => switchtab(p));
+        }
+        public void switchtab(MainWindow p)
+        {
+            int index = int.Parse(Name);
+            switch (index)
+            {
+                case 0:
+                    {
+                        p.Main.NavigationService.Navigate(new HomeView());
+                        break;
+                    }
+                case 1:
+                    {
+                        p.Main.NavigationService.Navigate(new OrderView());
+                        break;
+                    }
+                default:
+                    {
+                        break;
+                    }
+            }
+        }
+        public void moveWindow(MainWindow p)
+        {
+            p.DragMove();
+        }
+        public void Close()
+        {
+            System.Windows.Application.Current.Shutdown();
+        }
+        public void Minimize(MainWindow p)
+        {
+            p.WindowState = WindowState.Minimized;
+        }
+    }
+}
