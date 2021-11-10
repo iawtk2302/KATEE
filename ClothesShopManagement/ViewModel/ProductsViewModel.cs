0da9ee650abd1f194f76a74a7857b618aff3e92c
iawtk2302 <86036919+iawtk2302@users.noreply.github.com>
2021-11-10 20:49:20 +0700

    cập nhật

---
+++ ClothesShopManagement/ViewModel/ProductsViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClothesShopManagement.ViewModel
+{
+    public class Product
+    {
+        public string image { get; set; }
+        public string name { get; set; }
+        public string price { get; set; }
+        public Product()
+        {
+            image = "/Resource/Image/ao.jpg";
+            name = "Sample";
+            price = "350.000VND";
+        }
+    }
+    public class ProductsViewModel:BaseViewModel 
+    {
+        public ObservableCollection<Product> listproduct { get; set; }
+        public ProductsViewModel()
+        {
+            listproduct = new ObservableCollection<Product>();
+            listproduct.Add(new Product());
+            listproduct.Add(new Product());
+            listproduct.Add(new Product());
+            listproduct.Add(new Product());
+            listproduct.Add(new Product());
+            listproduct.Add(new Product());
+            listproduct.Add(new Product());
+            listproduct.Add(new Product());
+            listproduct.Add(new Product());
+            listproduct.Add(new Product());
+            listproduct.Add(new Product());
+            listproduct.Add(new Product());
+            listproduct.Add(new Product());
+            listproduct.Add(new Product());
+            listproduct.Add(new Product());
+            listproduct.Add(new Product());
+        }    
+    }
+}
