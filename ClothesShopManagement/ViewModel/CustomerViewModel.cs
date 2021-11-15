0aab188d46711889de1548207844307b9b074ae5
iawtk2302 <86036919+iawtk2302@users.noreply.github.com>
2021-11-16 00:21:29 +0700

    thêm view khách hàng

---
+++ ClothesShopManagement/ViewModel/CustomerViewModel.cs
@@ -0,0 +1,22 @@
+﻿using ClothesShopManagement.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ClothesShopManagement.ViewModel
+{
+    public class CustomerViewModel:BaseViewModel
+    {
+        private ObservableCollection<KHACHHANG> _listKH;
+        public ObservableCollection<KHACHHANG> listKH { get => _listKH; set { _listKH = value; OnPropertyChanged(); } }
+        public ICommand SearchCommand { get; set; }
+        public CustomerViewModel()
+        {
+            listKH = new ObservableCollection<KHACHHANG>(DataProvider.Ins.DB.KHACHHANGs);
+        }
+    }
+}
