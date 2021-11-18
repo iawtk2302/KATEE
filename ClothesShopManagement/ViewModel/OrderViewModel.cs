dfd318d54383fd45935ed4f9bafaa6d7ca2d234d
iawtk2302 <86036919+iawtk2302@users.noreply.github.com>
2021-11-18 23:12:36 +0700

    giao diện hóa đơn

--- ClothesShopManagement/ViewModel/OrderViewModel.cs
+++ ClothesShopManagement/ViewModel/OrderViewModel.cs
@@ -1,12 +1,20 @@
-﻿using System;
+﻿using ClothesShopManagement.Model;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ClothesShopManagement.ViewModel
 {
-    class OrderViewModel
+    public class OrderViewModel:BaseViewModel
     {
+        private ObservableCollection<HOADON> _listHD;
+        public ObservableCollection<HOADON> listHD { get => _listHD; set { _listHD = value; OnPropertyChanged(); } }
+        public OrderViewModel()
+        {
+            listHD= new ObservableCollection<HOADON>(DataProvider.Ins.DB.HOADONs);
+        }
     }
 }
