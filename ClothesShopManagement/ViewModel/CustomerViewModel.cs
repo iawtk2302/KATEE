65d80fe7fc1934b0de5b6f3608ed4958c1b2b31c
iawtk2302 <86036919+iawtk2302@users.noreply.github.com>
2021-11-18 13:25:49 +0700

    Thêm khách hàng

--- ClothesShopManagement/ViewModel/CustomerViewModel.cs
+++ ClothesShopManagement/ViewModel/CustomerViewModel.cs
@@ -16,11 +16,13 @@ namespace ClothesShopManagement.ViewModel
         public ObservableCollection<KHACHHANG> listKH { get => _listKH; set { _listKH = value; OnPropertyChanged(); } }
         public ICommand SearchCommand { get; set; }
         public ICommand Detail { get; set; }
+        public ICommand AddCs { get; set; }
         public CustomerViewModel()
         {
             listKH = new ObservableCollection<KHACHHANG>(DataProvider.Ins.DB.KHACHHANGs);
             SearchCommand = new RelayCommand<CustomerView>((p) => true, (p) => _SearchCommand(p));
             Detail = new RelayCommand<CustomerView>((p) => { return p.ListViewKH.SelectedItem == null ? false : true; }, (p) => _DetailCs(p));
+            AddCs = new RelayCommand<CustomerView>((p) => { return p.ListViewKH.SelectedItem == null ? false : true; }, (p) => _AddCs(p));
         }
         void _SearchCommand(CustomerView paramater)
         {
@@ -42,13 +44,35 @@ namespace ClothesShopManagement.ViewModel
         void _DetailCs(CustomerView paramater)
         {
             DetailCustomerView detailCustomerView = new DetailCustomerView();
-            detailCustomerView.ShowDialog();
             KHACHHANG temp=(KHACHHANG) paramater.ListViewKH.SelectedItem;
             detailCustomerView.MaKH.Text = temp.MAKH;
             detailCustomerView.TenKH.Text = temp.HOTEN;
             detailCustomerView.SDT.Text = temp.SDT;
             detailCustomerView.GT.Text = temp.GIOITINH;
-            
+            detailCustomerView.DC.Text = temp.DCHI;
+            int doanhso=0;
+            foreach(HOADON a in DataProvider.Ins.DB.HOADONs)
+            {
+                if (a.MAKH == temp.MAKH)
+                    doanhso += a.TRIGIA;
+            }    
+            detailCustomerView.DS.Text = doanhso.ToString();
+            string hang = "Đồng";
+            if (doanhso > 2000000 && doanhso <= 5000000)
+                hang = "Bạc";
+            else if (doanhso > 5000000 && doanhso <= 10000000)
+                hang = "Vàng";
+            else if(doanhso>10000000)
+                hang = "Kim cương";
+            detailCustomerView.Rank.Text = hang;
+            detailCustomerView.ShowDialog();
+            listKH = new ObservableCollection<KHACHHANG>(DataProvider.Ins.DB.KHACHHANGs);
+            paramater.ListViewKH.ItemsSource = listKH;
+        }
+        void _AddCs(CustomerView paramater)
+        {
+            AddCustomerView addCustomerView = new AddCustomerView();
+            addCustomerView.ShowDialog();
         }
     }
 }
--- ClothesShopManagement/ViewModel/DetailCustomer.cs
+++ ClothesShopManagement/ViewModel/DetailCustomer.cs
@@ -1,4 +1,5 @@
-﻿using ClothesShopManagement.View;
+﻿using ClothesShopManagement.Model;
+using ClothesShopManagement.View;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,23 +15,52 @@ namespace ClothesShopManagement.ViewModel
         public ICommand Closewd { get; set; }
         public ICommand Minimizewd { get; set; }
         public ICommand MoveWindow { get; set; }
+        private string MaKH;
+        public ICommand GetMAKH { get; set; }
+        public ICommand Update { get; set; }
         public DetailCustomer()
         {
             Closewd = new RelayCommand<DetailCustomerView>((p) => true, (p) => Close(p));
             Minimizewd = new RelayCommand<DetailCustomerView>((p) => true, (p) => Minimize(p));
             MoveWindow = new RelayCommand<DetailCustomerView>((p) => true, (p) => moveWindow(p));
+            GetMAKH = new RelayCommand<DetailCustomerView>((p) => true, (p) => _GetMAKH(p));
+            Update = new RelayCommand<DetailCustomerView>((p) => true, (p) => _Update(p));
         }
-        void moveWindow(DetailProduct p)
+        void moveWindow(DetailCustomerView p)
         {
             p.DragMove();
         }
-        void Close(DetailProduct p)
+        void Close(DetailCustomerView p)
         {
             p.Close();
         }
-        void Minimize(DetailProduct p)
+        void Minimize(DetailCustomerView p)
         {
             p.WindowState = WindowState.Minimized;
+        }
+        void _GetMAKH(DetailCustomerView paramater)
+        {
+            MaKH = paramater.MaKH.Text;
+        }
+        void _Update(DetailCustomerView p)
+        {
+            if(string.IsNullOrEmpty(p.TenKH.Text)|| string.IsNullOrEmpty(p.SDT.Text) || string.IsNullOrEmpty(p.GT.Text) || string.IsNullOrEmpty(p.DC.Text))
+            {
+                MessageBox.Show("Thông tin chưa đầy đủ !", "THÔNG BÁO");
+            }
+            else
+            {
+                var temp = DataProvider.Ins.DB.KHACHHANGs.Where(pa => pa.MAKH == MaKH);
+                foreach (KHACHHANG a in temp)
+                {
+                    a.HOTEN = p.TenKH.Text;
+                    a.SDT = p.SDT.Text;
+                    a.GIOITINH = p.GT.Text;
+                    a.DCHI = p.DC.Text;
+                }
+                DataProvider.Ins.DB.SaveChanges();
+                MessageBox.Show("Cập nhật thông tin thành công !", "THÔNG BÁO");
+            }           
         }
     }
 }
