b2b9bcfb6790d417a2446fe8f833c53571c1d58e
iawtk2302 <86036919+iawtk2302@users.noreply.github.com>
2021-11-19 13:31:44 +0700

    cập nhật

--- ClothesShopManagement/ViewModel/OrderViewModel.cs
+++ ClothesShopManagement/ViewModel/OrderViewModel.cs
@@ -15,15 +15,34 @@ namespace ClothesShopManagement.ViewModel
         private ObservableCollection<HOADON> _listHD;
         public ObservableCollection<HOADON> listHD { get => _listHD; set { _listHD = value; OnPropertyChanged(); } }
         public ICommand OpenAddOrder { get; set; }
+        public ICommand SearchCommand { get; set; }
         public OrderViewModel()
         {
             listHD= new ObservableCollection<HOADON>(DataProvider.Ins.DB.HOADONs);
             OpenAddOrder = new RelayCommand<OrderView>((p) => true, (p) => _OpenAdd(p));
+            SearchCommand = new RelayCommand<OrderView>((p) => true, (p) => _SearchCommand(p));
         }
         void _OpenAdd(OrderView paramater)
         {
             AddOrder addOrder = new AddOrder();
             addOrder.ShowDialog();
+        }
+        void _SearchCommand(OrderView paramater)
+        {
+            ObservableCollection<HOADON> temp = new ObservableCollection<HOADON>();
+            if (paramater.txbSearch.Text != "")
+            {
+                foreach (HOADON s in listHD)
+                {
+                    if (s.KHACHHANG.HOTEN.Contains(paramater.txbSearch.Text))
+                    {
+                        temp.Add(s);
+                    }
+                }
+                paramater.ListViewHD.ItemsSource = temp;
+            }
+            else
+                paramater.ListViewHD.ItemsSource = listHD;
         }
     }
 }
