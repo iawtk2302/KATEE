7c061912a827918396c8c2cfe3a77f376a39aa78
iawtk2302 <86036919+iawtk2302@users.noreply.github.com>
2021-11-19 12:12:06 +0700

    cập nhật

--- ClothesShopManagement/ViewModel/OrderViewModel.cs
+++ ClothesShopManagement/ViewModel/OrderViewModel.cs
@@ -1,10 +1,12 @@
 ﻿using ClothesShopManagement.Model;
+using ClothesShopManagement.View;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace ClothesShopManagement.ViewModel
 {
@@ -12,9 +14,16 @@ namespace ClothesShopManagement.ViewModel
     {
         private ObservableCollection<HOADON> _listHD;
         public ObservableCollection<HOADON> listHD { get => _listHD; set { _listHD = value; OnPropertyChanged(); } }
+        public ICommand OpenAddOrder { get; set; }
         public OrderViewModel()
         {
             listHD= new ObservableCollection<HOADON>(DataProvider.Ins.DB.HOADONs);
+            OpenAddOrder = new RelayCommand<OrderView>((p) => true, (p) => _OpenAdd(p));
+        }
+        void _OpenAdd(OrderView paramater)
+        {
+            AddOrder addOrder = new AddOrder();
+            addOrder.ShowDialog();
         }
     }
 }
