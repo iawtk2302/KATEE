20259fbfc043ae75e20a0f67d2c1406fd17f61f5
iawtk2302 <86036919+iawtk2302@users.noreply.github.com>
2021-11-23 13:06:08 +0700

    fix bug sản phẩm

--- ClothesShopManagement/Model/Const.cs
+++ ClothesShopManagement/Model/Const.cs
@@ -11,6 +11,7 @@ namespace ClothesShopManagement.Model
     {
         public static string TenDangNhap { get; set; }
         public static bool Admin { get; set; }
-        public static NGUOIDUNG ND { get; set; }   
+        public static NGUOIDUNG ND { get; set; }
+        public static string _localLink = System.Reflection.Assembly.GetExecutingAssembly().Location.Remove(System.Reflection.Assembly.GetExecutingAssembly().Location.IndexOf(@"bin\Debug"));
     }
 }
